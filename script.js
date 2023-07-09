09450c72043176600acba6820d71f2f4135edc36
anarchybox <139029683+anarchybox@users.noreply.github.com>
2023-07-09 09:14:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function generateText() {
-    document.getElementById("output").innerText = "Generated text goes here.";
+    var prompt = document.getElementById("prompt").value;
+
+    fetch('/generate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ prompt: prompt })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("output").innerText = data.generated_text;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
